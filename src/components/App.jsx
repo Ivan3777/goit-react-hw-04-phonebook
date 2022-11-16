8fe2f52d7ab422a3d4ccc59674494fb1683f507d
Ivan Vodopianov <vis3010@gmail.com>
2022-11-16 18:30:35 +0200

    end phonebook in react hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,79 +5,24 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Johnny Depp', number: '459-12-56' },
-    { id: 'id-2', name: 'Jared Leto', number: '443-89-12' },
-    { id: 'id-3', name: 'Mila Kunis', number: '645-17-79' },
-    { id: 'id-4', name: 'Robert Downey Jr', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem('contactList')) ?? [
+        { id: 'id-1', name: 'Johnny Depp', number: '459-12-56' },
+        { id: 'id-2', name: 'Jared Leto', number: '443-89-12' },
+        { id: 'id-3', name: 'Mila Kunis', number: '645-17-79' },
+        { id: 'id-4', name: 'Robert Downey Jr', number: '227-91-26' },
+      ]
+  );
   const [filter, setFilter] = useState('');
-  // const [name, setName] = useState('');
-  // const [number, setNumber] = useState('');
-
-  // state = {
-  // contacts: [
-  //   { id: 'id-1', name: 'Johnny Depp', number: '459-12-56' },
-  //   { id: 'id-2', name: 'Jared Leto', number: '443-89-12' },
-  //   { id: 'id-3', name: 'Mila Kunis', number: '645-17-79' },
-  //   { id: 'id-4', name: 'Robert Downey Jr', number: '227-91-26' },
-  // ],
-  //   filter: '',
-  //   name: '',
-  //   number: '',
-  // };
 
   useEffect(() => {
-    const contacts = localStorage.getItem('contactList');
-
-    if (contacts) {
-      const parsedContacts = JSON.parse(contacts);
-      setContacts(parsedContacts);
-    }
+    localStorage.setItem('contactList', JSON.stringify(contacts));
   }, [contacts]);
-
-  useEffect(() => {
-    const prevContacts = contacts;
-    const nextContacts = contacts;
-
-    if (prevContacts !== nextContacts) {
-      localStorage.setItem('contactList', JSON.stringify(nextContacts));
-    }
-  });
-
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contactList');
-
-  //   if (contacts) {
-  //     const parsedContacts = JSON.parse(contacts);
-  //     this.setState({ contacts: parsedContacts });
-  //   }
-  // }
-
-  // componentDidUpdate(_, prevState) {
-  //   const prevStateContacts = prevState.contacts;
-  //   const nextStayContacts = this.state.contacts;
-
-  //   if (prevStateContacts !== nextStayContacts) {
-  //     localStorage.setItem('contactList', JSON.stringify(nextStayContacts));
-  //   }
-  // }
 
   const handleChange = e => {
     const { value } = e.target;
     setFilter(value);
-    // const { name, value } = e.target;
-    // this.setState({ [name]: value });
-    // switch (name) {
-    //   case 'name':
-    //     setName(value);
-    //     break;
-    //   case 'number':
-    //     setNumber(value);
-    //     break;
-    //   default:
-    //     return;
-    // }
   };
 
   const handleSubmit = e => {
@@ -92,7 +37,7 @@ export const App = () => {
       contactsLists.push({ name, id, number });
     }
 
-    setContacts(contactsLists);
+    return setContacts(contactsLists);
   };
 
   const handleDelete = e => {
